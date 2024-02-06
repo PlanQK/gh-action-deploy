e2a1d8c2481a60d9ebe14fb7b1bef31393ba16e1
Michael Falkenthal <michael.falkenthal@anaqor.io>
2024-02-06 18:35:36 +0100

    chore: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ cd $GITHUB_WORKSPACE
 SERVICE_ID=$(awk -F'["]' '/"serviceId":/ {print $4}' planqk.json)
 if [ -n "$SERVICE_ID" ]; then
     npm install -g @anaqor/planqk
-    planqk login -t $INPUT_PLANQK_TOKEN
-    planqk set-context $INPUT_CONTEXT_ID
+    planqk login -t $PLANQK_TOKEN
+    planqk set-context $CONTEXT_ID
     planqk up --silent
     if [ $? -gt 0 ]; then
         echo "planqk up failed (return code not 0)."
