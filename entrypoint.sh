fd8a23ef3bb580ba84f89256a5453edc9b9548da
Michael Falkenthal <michael.falkenthal@anaqor.io>
2024-02-08 09:57:12 +0100

    chore: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-cd $GITHUB_WORKSPACE
-SERVICE_ID=$(awk -F'["]' '/"serviceId":/ {print $4}' planqk.json)
-if [ -n "$SERVICE_ID" ]; then
-    npm install -g @anaqor/planqk
-    planqk login -t $INPUT_PLANQK_TOKEN
-    planqk set-context $INPUT_CONTEXT_ID
-    planqk up --silent
-    if [ $? -gt 0 ]; then
-        echo "planqk up failed (return code not 0)."
-    fi
-else
-    echo "No service id given, aborting action"
+if [ -z "$INPUT_PLANQK_TOKEN" ]; then
+    echo "Input variable 'PLANQK_TOKEN' is not set, aborting action"
+    exit 1
 fi
+if [ -z "$INPUT_CONTEXT_ID" ]; then
+    echo "Input variable 'CONTEXT_ID' is not set, aborting action"
+    exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+
+SERVICE_ID=$(awk -F'["]' '/"serviceId":/ {print $4}' planqk.json)
+if [ -z "$SERVICE_ID" ]; then
+    echo "No service id found in planqk.json, aborting action"
+    exit 1
+fi
+
+npm install -g @anaqor/planqk
+planqk login -t $INPUT_PLANQK_TOKEN
+planqk set-context $INPUT_CONTEXT_ID
+planqk up --silent
+
+if [ "$?" != "0" ]; then
+    echo "Command 'planqk up' failed (return code $?)"
+fi
+
+exit $?
